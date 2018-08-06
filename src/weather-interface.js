189a41c4d13691507cf67b72094d7dcf5b2db23a
Guest User <Guest@epicodus-26.local>
2018-08-06 16:23:13 -0700

    have working jquery ajax api call

--- src/weather-interface.js
+++ src/weather-interface.js
@@ -1,3 +1,41 @@
-$(document).ready(function)(){
-  
-}
+// $(document).ready(function(){
+//   $('#weatherLocation').click(function(){
+//     let city = $('#location').val
+//     $('#location').val("");
+//     $.ajax({
+//       url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=ee010824608eb0b2064df3149ab5ba20`,
+//       type: 'GET',
+//       data: {
+//         format: 'json'
+//       },
+//       success: function(response) {
+//         $('.showHumidity').text('The humidity in $(city) is ${response.main.humidity}%');
+//         $('.showTemp').text('The temperature in Kelvins is ${response.main.temp}. ');
+//       },
+//     error: function() {
+//       $('#errors').text("There was an error processing your request. Please try again");
+//       }
+//     });
+//   });
+// });
+
+$(document).ready(function() {
+  $('#weatherLocation').click(function() {
+    let city = $('#location').val();
+    $('#location').val("");
+    $.ajax({
+      url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=ee010824608eb0b2064df3149ab5ba20`,
+      type: 'GET',
+      data: {
+        format: 'json'
+      },
+      success: function(response) {
+        $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+        $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp}.`);
+      },
+      error: function() {
+        $('#errors').text("There was an error processing your request. Please try again.");
+      }
+    });
+  });
+});
